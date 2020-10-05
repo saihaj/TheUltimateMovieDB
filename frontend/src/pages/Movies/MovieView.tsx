0b18a62409813536e775ea39492945f97802af96
saihaj <saihajpreet.singh@gmail.com>
2020-10-05 13:09:57 -0500

    feat: :art: style movie view page

--- frontend/src/pages/Movies/MovieView.tsx
+++ frontend/src/pages/Movies/MovieView.tsx
@@ -9,9 +9,11 @@ const MovieView: FC<PageProps> = () => {
 
   return (
     <Layout>
-      <span role="img" aria-labelledby="under construction page for movie">
-        🚧 You are watching future page for <b>{movieId}</b> 🚧
-      </span>
+      <div className="text-center text-xl md:text-2xl lg:text-3xl flex -mt-32 h-screen items-center justify-center">
+        <span role="img" aria-labelledby="under construction page for movie">
+          🚧 You are watching future page for <b>{movieId}</b> 🚧
+        </span>
+      </div>
     </Layout>
   )
 }
