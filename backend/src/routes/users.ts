dffbc8e35f2dc943d63c5027779bc2966d8a3010
Mihir Gupta <guptamihir418@gmail.com>
2020-10-23 01:08:46 +0530

    tried something

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,18 @@
 import { Router } from 'express'
+import { v4 as uuidv4 } from 'uuid';
 
+const users = [
+  {
+    id: '01',
+    name: 'Test',
+    role: 'Tester',
+  },
+  {
+    id: '02',
+    name: 'Test',
+    role: 'Tester',
+  },
+]
 const router = Router()
 
 /**
@@ -8,30 +21,24 @@ const router = Router()
  *  - name
  */
 router.get( '/', ( _, res ) => {
-  const users = [
-    {
-      name: 'Test',
-      role: 'Tester',
-    },
-    {
-      name: 'Test',
-      role: 'Tester',
-    },
-  ]
   res.json( users )
 } )
 
 // Get user by ID
 router.get( '/:user', ( { params: { user } }, res ) => {
-  console.log( user )
+  for ( let i = 0; i < users.length; i += 1 ) {
+    if ( users.id == user ) {
+      console.log( user )
+    }
+  }
   res.json( user )
 } )
 
 // Add a user
 router.post( '/', ( { params }, res ) => {
   const test = { ...params }
+  test.id = uuidv4()
+  test.name = params.name
   console.log( test )
   res.json( test )
 } )
-
-export default router
