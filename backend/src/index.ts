3a8d04ef8535469fbf887f2261d99fe850d82411
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-11-10 21:05:43 -0600

    build(backend): setup mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,29 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 import apiRoutes from './routes'
 import middleware from './utils/middleware'
 
 const app = express()
 
-app.use( middleware )
-app.use( apiRoutes )
+const startServer = async () => {
+  // Connect to DB
+  try {
+    await mongoose.connect( 'mongodb://root:toor@localhost:27017' )
+    console.log( 'Connected to MongoDB' )
+  } catch ( err ) {
+    console.log( err )
+    console.error( 'Unable to connect to MongoDB' )
+    process.exit( 126 )
+  }
 
-// Server
-app.listen( 4000, () => {
-  console.log( 'Server ready at http://localhost:4000/' )
-} )
+  app.use( middleware )
+  app.use( apiRoutes )
+
+  // Server
+  app.listen( 4000, () => {
+    console.log( 'Server ready at http://localhost:4000/' )
+  } )
+}
+
+startServer()
