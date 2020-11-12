49a6f096cba78b35f71990f35e8d82b3c6e1bb8a
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-11-11 21:16:08 -0600

    feat(backend): complete person route

--- backend/src/routes/people.ts
+++ backend/src/routes/people.ts
@@ -1,20 +1,44 @@
 import { Router } from 'express'
 
+import Models from '../models'
+import { DnE, GetItemById, GetAll } from '../utils/db'
+
 const router = Router()
 
 /**
  * Get all people
- * Search params supported:
- *  - name
  */
-router.get( '/', ( _, res ) => {
-  res.json( 'return a list of people' )
+router.get( '/', async ( _, res, next ) => {
+  try {
+    const person = await GetAll( Models.People )
+    return res.json( person );
+  } catch ( err ) { return next( err ) }
 } )
 
-// Get person by ID
-router.get( '/:person', ( { params: { person } }, res ) => {
-  console.log( person )
-  res.json( person )
+/**
+ * Get person by ID
+ */
+router.get( '/:personId', async ( { params: { personId } }, res, next ) => {
+  try {
+    const person = await GetItemById( Models.People, personId )
+    if ( person ) return res.json( person );
+    return next( DnE( personId ) );
+  } catch ( err ) { return next( err ) }
+} )
+
+/**
+ * Add a person to DB
+ *  - name: string
+ *  - director?: [Movie ObjectId]
+ *  - actor?: [Movie ObjectId]
+ *  - writer?: [Movie ObjectId]
+ */
+router.post( '/', async ( { body }, res, next ) => {
+  try {
+    const person = new Models.People( { ...body } )
+    const savePerson = await person.save()
+    return res.json( savePerson.toJSON() )
+  } catch ( err ) { return next( err ) }
 } )
 
 export default router
