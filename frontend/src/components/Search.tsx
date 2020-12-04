70041c29e84432a6de9bb1c34e4fba7319e79cb8
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-04 00:22:08 -0600

    feat(frontend): use api for search bar

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,23 +1,52 @@
-import React, { useState, DetailedHTMLProps, InputHTMLAttributes } from 'react'
+/* eslint-disable no-underscore-dangle */
+import React, { useState, DetailedHTMLProps, InputHTMLAttributes, useEffect } from 'react'
 import clsx from 'clsx'
+import { Link } from '@reach/router'
 
 type SearchBoxProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 const SearchBox = ( { ...props }: SearchBoxProps ) => {
   const [ searchValue, setSearchValue ] = useState( '' )
+  const [ results, setResults ] = useState( [] )
+
+  const handleChange = ( event:React.ChangeEvent<HTMLInputElement> ) => {
+    setSearchValue( event.target.value )
+  }
+
+  useEffect( () => {
+    if ( searchValue.length >= 2 && searchValue !== ' ' ) {
+      fetch( `/api/movies?title=${searchValue}` )
+        .then( res => res.json() )
+        .then( res => setResults( res ) )
+    }
+  }, [ searchValue ] )
 
   return (
-    <input
-      className={clsx(
-        'w-4/6 px-4 py-1 ',
-        'text-black',
-        'rounded-lg font-medium',
+    <>
+      <input
+        className={clsx(
+          'w-4/6 px-4 py-1',
+          'text-black',
+          'rounded-t-lg font-medium',
+        )}
+        value={searchValue}
+        onChange={handleChange}
+        placeholder="Search Movies"
+        {...props}
+      />
+      <div className={clsx(
+        'w-4/6 px-4 py-1',
+        'rounded-b-lg font-medium',
+        'bg-white text-black',
       )}
-      value={searchValue}
-      onChange={e => setSearchValue( e.target.value )}
-      placeholder="Search Movies"
-      {...props}
-    />
+      >
+        {results.map( ( a:{_id:string;title:string} ) => (
+          <div key={a._id} className="divide-y-4 divide-teal-800">
+            <Link className="hover:text-purple-900" to={`/movies/${a._id}`}>{a.title}</Link>
+          </div>
+        ) )}
+      </div>
+    </>
   )
 }
 
