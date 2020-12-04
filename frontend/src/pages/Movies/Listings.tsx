dc27859811df896da5b19dbbf0816c7949affcf6
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-04 00:22:42 -0600

    feat(frontend): update movie pages to consume new data

--- frontend/src/pages/Movies/Listings.tsx
+++ frontend/src/pages/Movies/Listings.tsx
@@ -1,43 +1,39 @@
-import React, { FC } from 'react'
+/* eslint-disable no-underscore-dangle */
+import React, { FC, useEffect, useRef, useState } from 'react'
 import { Link } from '@reach/router'
 import cx from 'clsx'
 import useSwr from 'swr'
 
 import { PageProps } from '../../lib/types'
 import Layout from '../../components/Layout'
+import useQuery from '../../hooks/use-query'
 
 type MovieCardProps = {
   /**
    * Movie's Title
    */
-  title: string
+  title: string;
   /**
    * Image URL for poster
    */
-  posterUrl: string
+  posterUrl: string;
   /**
    * Movie's director
    */
-  director: string
-  releaseDate: string
-  rated: string
-  movieId: string
-}
+  director: [{ name: string; _id: string }];
+  releaseDate: string;
+  movieId: string;
+};
 
-const MovieCard = (
-  {
-    movieId,
-    title,
-    posterUrl,
-    director,
-    releaseDate,
-    rated,
-  }: MovieCardProps,
-) => (
+const MovieCard = ( {
+  movieId,
+  title,
+  posterUrl,
+  director,
+  releaseDate,
+}: MovieCardProps ) => (
   <div className="p-2 md:w-1/3">
-
     <Link to={movieId}>
-
       <div
         className={cx(
           'h-full overflow-hidden mx-auto',
@@ -45,65 +41,89 @@ const MovieCard = (
           'lg:w-11/12 md:w-full w-2/3',
         )}
       >
-        <div style={{ height: 'inherit' }} className="flex flex-col justify-between">
-
+        <div
+          style={{ height: 'inherit' }}
+          className="flex flex-col justify-between"
+        >
           <h1 className="text-3xl font-bold text-center">{title}</h1>
           <img src={posterUrl} alt={`Poster for ${title}`} />
 
           <div className="flex justify-between">
-
             <p className="text-lg ml-2 my-2">
-              Director: <b>{director}</b>
+              Director:
+              {director.map( ( { name, _id: id } ) => (
+                <b key={id}>{name}</b>
+              ) )}
               <br />
               Released on: {releaseDate}
             </p>
-
-            <div className="my-auto mr-2">
-              <h6
-                className={cx(
-                  'text-yellow-400 border-2 border-yellow-400 rounded-full',
-                  'px-2 py-1',
-                )}
-              >
-                {rated}
-              </h6>
-            </div>
-
           </div>
-
         </div>
       </div>
-
     </Link>
-
   </div>
 )
 
 const Listings: FC<PageProps> = () => {
-  const { data, error } = useSwr( '/api/movies' )
+  const [ mounted, setMounted ] = useState( false )
+  const offset = useQuery( 'offset' ) || 0
+  const title = useQuery( 'title' ) || undefined
+  const genre = useQuery( 'genre' ) || undefined
+  const apiQueryString = '/api/movies?limit=9'
+  const queryString = useRef( apiQueryString )
+
+  useEffect( () => {
+    const cleanQueryParam = () => {
+      if ( title && genre ) {
+        return `${apiQueryString}&title=${title}&genre=${genre}`
+      }
+
+      if ( title ) {
+        return `${apiQueryString}&title=${title}`
+      }
+
+      if ( genre ) {
+        return `${apiQueryString}&genre=${genre}`
+      }
+
+      return `${apiQueryString}&offset=${offset}`
+    }
+
+    queryString.current = cleanQueryParam()
+    setMounted( true )
+  }, [ title, genre, offset, mounted ] )
+
+  const { data, error } = useSwr( mounted ? queryString.current : null )
 
   return (
     <Layout nav>
       {!error && !data && <div>Loading...</div>}
-      {error && <h1 className="text-center text-3xl pt-16">We have a problem!</h1>}
+      {error && (
+      <h1 className="text-center text-3xl pt-16">We have a problem!</h1>
+      )}
 
       {data && (
       <div className="md:flex md:flex-wrap">
-        {/* @ts-ignore */}
-          {data.map( ( { title, poster, id, director, released, rated } ) => (
-            <MovieCard
-              key={id}
-              movieId={id}
-              title={title}
-              posterUrl={poster}
-              director={director}
-              releaseDate={released}
-              rated={rated}
-            />
-          ) )}
+        {data.map(
+          ( a: {
+              _id: MovieCardProps['movieId'];
+              title: MovieCardProps['title'];
+              poster: MovieCardProps['posterUrl'];
+              directors: MovieCardProps['director'];
+              meta: { releaseDate: MovieCardProps['releaseDate'] };
+            } ) => (
+              <MovieCard
+                key={a._id}
+                movieId={a._id}
+                title={a.title}
+                posterUrl={a.poster}
+                director={a.directors}
+                releaseDate={a.meta.releaseDate}
+              />
+          ),
+        )}
       </div>
       )}
-
     </Layout>
   )
 }
--- frontend/src/pages/Movies/MovieView.tsx
+++ frontend/src/pages/Movies/MovieView.tsx
@@ -1,26 +1,59 @@
 import React, { FC } from 'react'
-import { useParams } from '@reach/router'
+import { useParams, Link } from '@reach/router'
 import useSWR from 'swr'
+// @ts-expect-error ignore types for this
+import ReactJoin from 'react-join'
 
 import { PageProps } from '../../lib/types'
 import Layout from '../../components/Layout'
 
-type MovieComponentProps = {
-  title: string
-  posterUrl: string
-  director: string
-}
+type PeopleNamesProps = {
+  list: [{ _id: string; name: string }];
+};
 
-const MovieComponent = ( { title, posterUrl, director }: MovieComponentProps ) => (
-  <div className="md:flex md:justify-around">
+type MovieComponentProps = {
+  title: string;
+  posterUrl: string;
+  directors: PeopleNamesProps['list'];
+  actors: PeopleNamesProps['list'];
+  writers: PeopleNamesProps['list'];
+};
+
+const PeopleNames = ( { list }: PeopleNamesProps ) => (
+  <ReactJoin separator={<span>, </span>}>
+    {list.map( ( { _id: id, name } ) => (
+      <Link className="hover:text-yellow-400" to={`/people/${id}`} key={id}>
+        {name}
+      </Link>
+    ) )}
+  </ReactJoin>
+)
+
+const MovieComponent = ( {
+  title,
+  posterUrl,
+  directors,
+  actors,
+  writers,
+}: MovieComponentProps ) => (
+  <div className="md:flex">
     <img
       alt={`Posted of ${title}`}
       src={posterUrl}
-      className="lg:w-1/2 md:w-2/5 w-4/6 rounded-lg mx-auto md:mx-0"
+      className="lg:w-1/2 md:w-2/5 w-4/6 rounded-lg mx-auto md:mx-0 md:pr-10"
     />
     <div className="flex flex-col my-auto text-center md:text-left">
       <h1 className="text-5xl font-bold">{title}</h1>
-      <h3 className="text-2xl font-medium">{director}</h3>
+      <h3 className="text-2xl font-medium">
+        <b>Director: </b>
+        <PeopleNames list={directors} />
+        <br />
+        <b>Actors: </b>
+        <PeopleNames list={actors} />
+        <br />
+        <b>Writers: </b>
+        <PeopleNames list={writers} />
+      </h3>
     </div>
   </div>
 )
@@ -28,19 +61,24 @@ const MovieComponent = ( { title, posterUrl, director }: MovieComponentProps ) =
 const MovieView: FC<PageProps> = () => {
   const { movieId } = useParams()
   const { data, error } = useSWR( `/api/movies/${movieId}` )
-
   return (
     <Layout nav>
       {!error && !data && <div>Loading...</div>}
-      {error && <h1 className="text-center text-3xl pt-16">We have a problem! Movie not found</h1>}
+      {error && (
+      <h1 className="text-center text-3xl pt-16">
+        We have a problem! Movie not found
+      </h1>
+      )}
       {data && (
-        <div className="max-w-2xl mx-auto pt-32">
-          <MovieComponent
-            title={data.title}
-            posterUrl={data.poster}
-            director={data.director}
-          />
-        </div>
+      <div className="max-w-3xl mx-auto pt-32">
+        <MovieComponent
+          title={data.title}
+          posterUrl={data.poster}
+          directors={data.directors}
+          actors={data.actors}
+          writers={data.writers}
+        />
+      </div>
       )}
     </Layout>
   )
