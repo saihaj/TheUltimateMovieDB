6af6fda2d7de33a6a59167fa7db9288a6a86934f
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-05 11:32:43 -0600

    feat(backend): minrating query param

--- backend/README.md
+++ backend/README.md
@@ -1,11 +1,13 @@
 # Backend
 
 ## Available Scripts
+
 `npm run [command]`
 Available Commands
+
 ```
 lint        Run eslint
-dev         Run TypeScript watcher and nodemon parallel 
+dev         Run TypeScript watcher and nodemon parallel
 build       Create a production build
 clean       Using rimraf remove old build
 start       Run clean, build and then start the server
@@ -13,12 +15,14 @@ start       Run clean, build and then start the server
 
 ## Endpoints
 
-NOTE: Users and People route use mongo db to store everything. Movies route are not yet moved to use DB it still uses  [`movie-data-short.json`](../dataset/movie-data-short.json) for data and the data is stored in RAM for this route only.
+NOTE: Users and People route use mongo db to store everything. Movies route are not yet moved to use DB it still uses [`movie-data-short.json`](../dataset/movie-data-short.json) for data and the data is stored in RAM for this route only.
 
 I did setup mongo schema for all the routes see [`models`](./src/models/index.ts)
 
 ### GET
+
 - `users`
+
   - Following fields should be returned by all endpoints in this route. Check for exceptions or additional fields for that route. Most accurate object return type in [users model](./src/models/user.ts)
     - `_id`
     - `name`
@@ -33,6 +37,7 @@ I did setup mongo schema for all the routes see [`models`](./src/models/index.ts
     - Returns a user given an ID
 
 - `people`
+
   - Following fields should be returned by all endpoints in this route. Check for exceptions or additional fields for that route.
     - `_id`
     - `name`
@@ -43,32 +48,38 @@ I did setup mongo schema for all the routes see [`models`](./src/models/index.ts
     - Returns and array of people objects
     - `/:personId`
       - Returns a people object
-      - 
+      -
 
 - `movies`
+
   - Following fields should be returned by all endpoints in this route. Check for exceptions or additional fields for that route.
   - Most accurate object return type in [movie model](./src/models/movie.ts)
   - `/`
+
     - Query params accepted
       - title
       - genre
       - year
-      - limit 
+      - minrating
+      - limit
         - default: 10
         - max: 50
       - offset
         - default: 0
     - Returns
+
       - ```json
-          {   
-            info: {
+        {
+          "info": {
             limit,
-            nextOffset: offset + limit,
-            },
-            results: [movie],
-          }
-      ```
+            "nextOffset": offset + limit
+          },
+          "results": [movie]
+        }
+        ```
+
     - Movies are returned in ascending order as in DB
+
   - `/:movie`
     - Returns a movie
   - `/reviews/:movie`
@@ -77,6 +88,7 @@ I did setup mongo schema for all the routes see [`models`](./src/models/index.ts
     - Returns array of reviews. See [movie rating schema](./src/models/movie.ts).
 
 ### POST
+
 - `movies`
   - `/`
     - Returns a success message. You must specify the following fields.
@@ -131,6 +143,7 @@ I did setup mongo schema for all the routes see [`models`](./src/models/index.ts
     - This will decode the JWT token
 
 ### PATCH
+
 - `users`
   - `/:userId/role`
     - Updates the role of user
--- backend/src/routes/movies/index.ts
+++ backend/src/routes/movies/index.ts
@@ -1,21 +1,22 @@
 /* eslint-disable no-underscore-dangle */
-import { Router } from 'express'
-import { v4 } from 'uuid'
+import { Router } from 'express';
+import { v4 } from 'uuid';
 
-import Models from '../../models'
-import dataset from '../../movie-data'
-import { NumChecking, EscapeRegex, GetItemById, DnE } from '../../utils/db'
+import Models from '../../models';
+import dataset from '../../movie-data';
+import { NumChecking, EscapeRegex, GetItemById, DnE } from '../../utils/db';
 
-import Ratings from './ratings'
-import Reviews from './review'
+import Ratings from './ratings';
+import Reviews from './review';
 
-const router = Router()
+const router = Router();
 
 type SearchParamMovies = {
-  year?:string
-  title?:string | RegExp
-  genre?:string
-}
+  year?: string;
+  title?: string | RegExp;
+  genre?: string;
+  'score.average'?: { $gte: Number };
+};
 
 /**
  * Get all movies
@@ -23,6 +24,7 @@ type SearchParamMovies = {
  *   - title
  *   - genre
  *   - year
+ *   - minrating
  * query params
  *    - limit: default 10 max 50
  *    - offset: default 0
@@ -31,25 +33,34 @@ type SearchParamMovies = {
  */
 router.get( '/', async ( { query }, res, next ) => {
   try {
-    const limit = NumChecking( parseInt( query.limit as string, 10 ), 10, 50 )
-    const offset = NumChecking( parseInt( query.offset as string, 10 ), 0 )
+    const limit = NumChecking( parseInt( query.limit as string, 10 ), 10, 50 );
+    const offset = NumChecking( parseInt( query.offset as string, 10 ), 0 );
 
-    const searchParams:SearchParamMovies = {}
+    const searchParams: SearchParamMovies = {};
 
     if ( query.year ) {
-      searchParams.year = query.year as string
+      searchParams.year = query.year as string;
     }
 
     if ( query.title ) {
-      searchParams.title = new RegExp( EscapeRegex( query.title as string ), 'gi' )
+      searchParams.title = new RegExp( EscapeRegex( query.title as string ), 'gi' );
     }
 
     if ( query.genre ) {
-      searchParams.genre = query.genre as string
+      searchParams.genre = query.genre as string;
     }
 
-    const movies = await Models.MovieModel
-      .find( searchParams, null, { sort: { title: 1 }, skip: offset, limit } ).populate( 'meta directors' )
+    if ( query.minrating ) {
+      searchParams[ 'score.average' ] = {
+        $gte: parseInt( query.minrating as string, 10 ),
+      };
+    }
+
+    const movies = await Models.MovieModel.find( searchParams, null, {
+      sort: { title: 1 },
+      skip: offset,
+      limit,
+    } ).populate( 'meta directors' );
 
     return res.json( {
       info: {
@@ -58,8 +69,10 @@ router.get( '/', async ( { query }, res, next ) => {
       },
       results: movies,
     } );
-  } catch ( err ) { return next( err ) }
-} )
+  } catch ( err ) {
+    return next( err );
+  }
+} );
 
 /**
  * Get movie by ID
@@ -68,56 +81,72 @@ router.get( '/', async ( { query }, res, next ) => {
 router.get( '/:movie', async ( { params: { movie } }, res, next ) => {
   try {
     // @ts-expect-error populate type errors
-    const movieObj = await GetItemById( Models.MovieModel, movie, 'directors actors writers' )
+    const movieObj = await GetItemById(
+      Models.MovieModel,
+      movie,
+      'directors actors writers',
+    );
     if ( movieObj ) return res.json( movieObj );
     return next( DnE( movie ) );
-  } catch ( err ) { return next( err ) }
-} )
+  } catch ( err ) {
+    return next( err );
+  }
+} );
 
 // Post a movie
 router.post( '/', ( { body }, res ) => {
-  const input = { id: v4(), ...body }
-  dataset.push( input )
-  res.status( 200 ).json( { message: 'Successfully added the movie' } )
-} )
+  const input = { id: v4(), ...body };
+  dataset.push( input );
+  res.status( 200 ).json( { message: 'Successfully added the movie' } );
+} );
 
 /**
  * Score a movie
  * For Return type of object see `MovieSchema`
  */
-router.patch( '/score/:movie', async ( { params: { movie }, body }, res, next ) => {
-  if ( !body.score ) return next( { message: '`score` is a required field.', status: 412 } );
-  if ( body.score > 10 || body.score < 0 ) return next( { message: '`score` needs to be between 0-10.', status: 412 } );
-
-  try {
-    const movieObj = await GetItemById( Models.MovieModel, movie )
-
-    if ( movieObj ) {
-      const newScore = movieObj.score.total + +body.score;
-      const newCounter = movieObj.score.count + 1;
-      const newAvg = newScore / newCounter;
-
-      await Models.MovieModel.findByIdAndUpdate(
-        { _id: movie },
-        {
-          $set: {
-            score: {
-              total: newScore,
-              count: newCounter,
-              average: newAvg,
-            },
-          },
-        },
-      ).exec();
-
-      return res.json( { message: `Average score is ${newAvg}` } );
+router.patch(
+  '/score/:movie',
+  async ( { params: { movie }, body }, res, next ) => {
+    if ( !body.score ) return next( { message: '`score` is a required field.', status: 412 } );
+    if ( body.score > 10 || body.score < 0 ) {
+      return next( {
+        message: '`score` needs to be between 0-10.',
+        status: 412,
+      } );
     }
 
-    return next( { message: `${movie} does not exist`, status: 404 } );
-  } catch ( err ) { return next( err ); }
-} );
+    try {
+      const movieObj = await GetItemById( Models.MovieModel, movie );
 
-router.use( '/rating', Ratings )
-router.use( '/review', Reviews )
+      if ( movieObj ) {
+        const newScore = movieObj.score.total + +body.score;
+        const newCounter = movieObj.score.count + 1;
+        const newAvg = newScore / newCounter;
 
-export default router
+        await Models.MovieModel.findByIdAndUpdate(
+          { _id: movie },
+          {
+            $set: {
+              score: {
+                total: newScore,
+                count: newCounter,
+                average: newAvg,
+              },
+            },
+          },
+        ).exec();
+
+        return res.json( { message: `Average score is ${newAvg}` } );
+      }
+
+      return next( { message: `${movie} does not exist`, status: 404 } );
+    } catch ( err ) {
+      return next( err );
+    }
+  },
+);
+
+router.use( '/rating', Ratings );
+router.use( '/review', Reviews );
+
+export default router;
