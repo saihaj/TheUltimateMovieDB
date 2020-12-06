a2238d1e8dc268c4363d7ee3ed591211580032cb
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-12-06 09:18:04 -0600

    feat(frontend): allow custom style

--- frontend/src/components/ProfileActionButton.tsx
+++ frontend/src/components/ProfileActionButton.tsx
@@ -9,14 +9,16 @@ type ActionButtonProps = {
    */
   action: () => void,
   label: string
+  customStyle?: string
 }
 
-const ActionButton = ( { action, label }:ActionButtonProps ) => (
+const ActionButton = ( { action, label, customStyle }:ActionButtonProps ) => (
   <div
     className={clsx(
       'bg-gray-400 text-indigo-800',
       'px-4 py-1 rounded-lg',
       'hover:bg-yellow-400',
+      customStyle,
     )}
     style={{ cursor: 'pointer' }}
     onClick={action}
